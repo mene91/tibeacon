48db63c0c6d13c8346d3b718f44dbdd1a9640a69
m1ga <miga_miga@gmx.de>
2014-06-03 20:10:47 +0200

    stop crashing if called from alarmmanager (will stop scanning too)

--- android/src/miga/tibeacon/TibeaconModule.java
+++ android/src/miga/tibeacon/TibeaconModule.java
@@ -38,31 +38,33 @@ public class TibeaconModule extends KrollModule implements IBeaconListener{
 	Timer timer = new Timer();
 	boolean isRunning = false;
 
-       @Override
-        public void onDestroy(Activity activity) {
-	  Log.d("BEACON","destroy");
-	  super.onDestroy(activity);
-        }
+	@Override
+	public void onDestroy(Activity activity) {
+		Log.d("BEACON","destroy");
+		super.onDestroy(activity);
+	}
 
-       @Override
-        public void onResume(Activity activity) {
-         super.onResume(activity);
-	  Log.d("BEACON","resume");
-        }
+	@Override
+	public void onResume(Activity activity) {
+		super.onResume(activity);
+		Log.d("BEACON","resume");
+	}
 
 
-       @Override
-        public void onStart(Activity activity) {
-	  super.onStart(activity);
-	  Log.d("BEACON","start");
-        }
+	@Override
+	public void onStart(Activity activity) {
+		super.onStart(activity);
+		Log.d("BEACON","start");
+	}
 
 	public TibeaconModule () {
 		super();
 		TiApplication appContext = TiApplication.getInstance();
-		activity = appContext.getCurrentActivity();
-		context=activity.getApplicationContext();
-
+		activity = appContext.getAppRootOrCurrentActivity();
+		Log.i("LOG","act:" + activity);
+		Log.i("LOG","act:" + appContext.getAppCurrentActivity());
+		Log.i("LOG","act:" +  appContext.getCurrentActivity());
+		context = appContext.getBaseContext();
 	}
 
 
@@ -73,14 +75,12 @@ public class TibeaconModule extends KrollModule implements IBeaconListener{
 
 	@Kroll.method
 	public void initBeacon(HashMap args){
-	    KrollDict arg = new KrollDict(args);
-	    success =(KrollFunction) arg.get("success");
-	    region=(KrollFunction) arg.get("region");
-	    error =(KrollFunction) arg.get("error");
-	    found =(KrollFunction) arg.get("found");
-	    seconds=arg.optInt("interval",60);
-
-
+		KrollDict arg = new KrollDict(args);
+		success =(KrollFunction) arg.get("success");
+		region=(KrollFunction) arg.get("region");
+		error =(KrollFunction) arg.get("error");
+		found =(KrollFunction) arg.get("found");
+		seconds=arg.optInt("interval",60);
 	}
 
 
@@ -109,10 +109,10 @@ public class TibeaconModule extends KrollModule implements IBeaconListener{
 		HashMap<String, KrollDict> event = new HashMap<String, KrollDict>();
 		event.put("device", generateCallback(ibeacon));
 
-	      // Success-Callback
-	      if (found!=null){
-		found.call(getKrollObject(), event);
-	      }
+		// Success-Callback
+		if (found!=null){
+			found.call(getKrollObject(), event);
+		}
 	}
 
 	@Override
@@ -120,116 +120,123 @@ public class TibeaconModule extends KrollModule implements IBeaconListener{
 		activity.runOnUiThread(new Runnable() {
 			@Override
 			public void run() {}
-		});
-	}
-
-	@Override
-	public void enterRegion(IBeacon ibeacon) {
-		Log.d("BEACON","Enter region: " + ibeacon.toString());
-
-		HashMap<String, KrollDict> event = new HashMap<String, KrollDict>();
-		event.put("device", generateCallback(ibeacon));
-
-	      // Success-Callback
-	      if (region!=null){
-		region.call(getKrollObject(), event);
-	      }
-
-	}
-
-	@Kroll.method
-	public void startScanning(){
-	    ibp = IBeaconProtocol.getInstance(activity);
-	    ibp.setListener(this);
-	    TimerTask searchIbeaconTask = new TimerTask() {
-			@Override
-			public void run() {
-				Log.d("BEACON","timer run");
-				activity.runOnUiThread(new Runnable() {
-					@Override
-					public void run() {
-						scanBeacons();
-					}
-				});
-			}
-		};
-		timer = new Timer();
-		timer.scheduleAtFixedRate(searchIbeaconTask, 1000, seconds*1000);
-	}
-
-	@Kroll.method
-	public void stopScanning(){
-	  Log.d("BEACON","stop scanning");
-	  isRunning =false;
-	  timer.cancel();
-	}
-
-
-	@Kroll.method
-	public boolean isEnabled(){
-	  ibp = IBeaconProtocol.getInstance(activity);
-	  return IBeaconProtocol.initializeBluetoothAdapter(activity);
-	}
-
-
-	@Kroll.method
-	public boolean isRunning(){
-	  return isRunning;
-	}
-
-	private void scanBeacons(){
-		// Check Bluetooth every time
-		isRunning =true;
-		Log.d("BEACON","Scanning...");
-		ibp = IBeaconProtocol.getInstance(activity);
-
-		// Filter based on default easiBeacon UUID, remove if not required
-		//ibp.setScanUUID(UUID);
-
-		if(!IBeaconProtocol.initializeBluetoothAdapter(activity)){
-			Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-			activity.startActivityForResult(enableBtIntent, REQUEST_BLUETOOTH_ENABLE );
-
-		} else{
-			ibp.setListener(this);
-			if(ibp.isScanning())
-				ibp.scanIBeacons(false);
-			ibp.reset();
-			ibp.scanIBeacons(true);
+			});
 		}
+
+		@Override
+		public void enterRegion(IBeacon ibeacon) {
+			Log.d("BEACON","Enter region: " + ibeacon.toString());
+
+			HashMap<String, KrollDict> event = new HashMap<String, KrollDict>();
+			event.put("device", generateCallback(ibeacon));
+
+			// Success-Callback
+			if (region!=null){
+				region.call(getKrollObject(), event);
+			}
+
+		}
+
+		@Kroll.method
+		public void startScanning(){
+			ibp = IBeaconProtocol.getInstance(context);
+			ibp.setListener(this);
+
+
+			TimerTask searchIbeaconTask = new TimerTask() {
+				@Override
+				public void run() {
+					Log.d("BEACON","timer run");
+
+					if (activity!=null){
+						activity.runOnUiThread(new Runnable() {
+							@Override
+							public void run() {
+								scanBeacons();
+							}
+						});
+					} else {
+						// started from alarmanager
+					}
+				}
+			};
+			timer = new Timer();
+			timer.scheduleAtFixedRate(searchIbeaconTask, 1000, seconds*1000);
+		}
+
+		@Kroll.method
+		public void stopScanning(){
+			Log.d("BEACON","stop scanning");
+			isRunning =false;
+			timer.cancel();
+		}
+
+
+		@Kroll.method
+		public boolean isEnabled(){
+			ibp = IBeaconProtocol.getInstance(context);
+			return IBeaconProtocol.initializeBluetoothAdapter(context);
+		}
+
+
+		@Kroll.method
+		public boolean isRunning(){
+			return isRunning;
+		}
+
+		private void scanBeacons(){
+			// Check Bluetooth every time
+			isRunning =true;
+			Log.d("BEACON","Scanning...");
+			ibp = IBeaconProtocol.getInstance(context);
+
+			// Filter based on default easiBeacon UUID, remove if not required
+			//ibp.setScanUUID(UUID);
+
+			if(!IBeaconProtocol.initializeBluetoothAdapter(context)){
+				Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+				activity.startActivityForResult(enableBtIntent, REQUEST_BLUETOOTH_ENABLE );
+
+			} else{
+				ibp.setListener(this);
+				if(ibp.isScanning())
+				ibp.scanIBeacons(false);
+				ibp.reset();
+				ibp.scanIBeacons(true);
+			}
+		}
+
+		public KrollDict generateCallback(IBeacon beacon){
+			KrollDict d = new KrollDict();
+			d.put("mac",beacon.getMacAddress());
+			d.put("major",beacon.getMajor());
+			d.put("minor",beacon.getMinor());
+			d.put("rssi",beacon.getRssiValue());
+			d.put("power",beacon.getPowerValue());
+			d.put("proximity",beacon.getProximity());
+			d.put("uuid",beacon.getUuidHexString());
+			d.put("uuidDashed",beacon.getUuidHexStringDashed());
+			d.put("type",beacon.getType());
+			return d;
+		}
+
+		public void sendData(){
+
+			ArrayList<IBeacon> beacons = ibp.getIBeaconsByProximity();
+			HashMap<String, KrollDict[]> event = new HashMap<String, KrollDict[]>();
+			KrollDict[] dList = new KrollDict[beacons.size()];
+
+			Log.d("BEACON","returning: "+beacons.size());
+			for (int i=0; i<beacons.size();++i){
+				IBeacon beacon = beacons.get(i);
+				dList[i]=generateCallback(beacon);
+			}
+			event.put("devices", dList);
+
+			// Success-Callback
+			if (success!=null){
+				success.call(getKrollObject(), event);
+			}
+		}
+
 	}
-
-	public KrollDict generateCallback(IBeacon beacon){
-	    KrollDict d = new KrollDict();
-	    d.put("mac",beacon.getMacAddress());
-	    d.put("major",beacon.getMajor());
-	    d.put("minor",beacon.getMinor());
-	    d.put("rssi",beacon.getRssiValue());
-	    d.put("power",beacon.getPowerValue());
- 	   d.put("proximity",beacon.getProximity());
- 	   d.put("uuid",beacon.getUuidHexString());
- 	   d.put("uuidDashed",beacon.getUuidHexStringDashed());
- 	   d.put("type",beacon.getType());
- 	   return d;
-	}
-
-	public void sendData(){
-
-	  ArrayList<IBeacon> beacons = ibp.getIBeaconsByProximity();
-	  HashMap<String, KrollDict[]> event = new HashMap<String, KrollDict[]>();
-	  KrollDict[] dList = new KrollDict[beacons.size()];
-
-	  Log.d("BEACON","returning: "+beacons.size());
-	  for (int i=0; i<beacons.size();++i){
-	    IBeacon beacon = beacons.get(i);
-	    dList[i]=generateCallback(beacon);
-	  }
-	  event.put("devices", dList);
-
-	  // Success-Callback
-	  if (success!=null){
-	    success.call(getKrollObject(), event);
-	  }
-	}
-
-}
